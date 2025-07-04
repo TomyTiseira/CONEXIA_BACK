3d227e5f06a340a80e7d456d584eda699012e078
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-04 18:13:16 -0300

    fix: docker funcionando para windows

--- users/entrypoint.sh
+++ users/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+set -e
 
 # Ejecutar seeds
+echo "Ejecutando seeds..."
 npm run seed:roles
 npm run seed:document-types
 
 # Iniciar la aplicación
+echo "Iniciando aplicación..."
 npm run start:dev 
