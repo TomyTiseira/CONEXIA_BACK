3e54d14dd6f60c48ce0daa153caffa5790d280f1
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-06 12:56:03 -0300

    refactor: Eliminar la lógica de creación de usuario en el script de inicialización de Postgres y añadir la recarga de configuración. Se simplifica el proceso de configuración al eliminar permisos y creación de usuario, manteniendo solo la recarga de la configuración.

--- users/postgres-init/01-init.sql
+++ users/postgres-init/01-init.sql
@@ -2,20 +2,5 @@
 ALTER SYSTEM SET listen_addresses = '*';
 ALTER SYSTEM SET max_connections = 100;
 
--- Crear el usuario si no existe
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = current_setting('POSTGRES_USER')) THEN
-        CREATE USER current_setting('POSTGRES_USER') WITH PASSWORD current_setting('POSTGRES_PASSWORD');
-    END IF;
-END
-$$;
-
--- Dar permisos al usuario
-GRANT ALL PRIVILEGES ON DATABASE current_setting('POSTGRES_DB') TO current_setting('POSTGRES_USER');
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO current_setting('POSTGRES_USER');
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO current_setting('POSTGRES_USER');
-GRANT ALL PRIVILEGES ON SCHEMA public TO current_setting('POSTGRES_USER');
-
--- Configurar pg_hba.conf para permitir conexiones desde cualquier host
--- Esto se hace a través de variables de entorno en el docker-compose 
+-- Recargar la configuración
+SELECT pg_reload_conf(); 
