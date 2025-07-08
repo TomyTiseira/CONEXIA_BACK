beeeabaf4d2b1da278f4a16dd4901e4e5951bfb1
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-08 19:59:46 -0300

    feat: Añadir soporte para CORS configurando CORS_ORIGINS en envs y actualizando la configuración de CORS en el api-gateway.

--- api-gateway/src/config/envs.ts
+++ api-gateway/src/config/envs.ts
@@ -5,20 +5,22 @@ interface EnvVars {
   NATS_SERVERS: string[];
   PORT: number;
   JWT_SECRET: string;
-}
+  CORS_ORIGINS?: string[];
+}0
 
 const envSchema = joi
   .object({
     NATS_SERVERS: joi.array().items(joi.string()).required(),
     PORT: joi.number().required(),
     JWT_SECRET: joi.string().required(),
-  })
+    CORS_ORIGINS: joi.string().optional()})
   .unknown(true);
 
 const result = envSchema.validate({
   NATS_SERVERS: process.env.NATS_SERVERS?.split(',') || [],
   PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
   JWT_SECRET: process.env.JWT_SECRET,
+  CORS_ORIGINS: process.env.CORS_ORIGINS?.split(',') || [],
 });
 if (result.error) {
   throw new Error(`Config validation error: ${result.error.message}`);
@@ -30,4 +32,5 @@ export const envs = {
   natsServers: envVars.NATS_SERVERS,
   port: envVars.PORT,
   jwtSecret: envVars.JWT_SECRET,
+  corsOrigins: envVars.CORS_ORIGINS || [],
 };
