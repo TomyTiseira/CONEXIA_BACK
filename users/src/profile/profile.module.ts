60042fb811e5908e6ef209b0ec2813135c2131b9
Juan Cruz <juanperalta_lr@hotmail.com>
2025-07-09 12:28:40 -0300

    feat: import CreateProfileUseCase in profile.module.ts

--- users/src/profile/profile.module.ts
+++ users/src/profile/profile.module.ts
@@ -5,11 +5,12 @@ import { ProfileController } from './controller/profile.controller';
 import { Profile } from './entities/profile.entity';
 import { ProfileRepository } from './repository/profile.repository';
 import { ProfileService } from './service/profile.service';
+import { CreateProfileUseCase } from './service/use-cases/create-profile.use-cases';
 
 @Module({
   imports: [SharedModule, TypeOrmModule.forFeature([Profile])],
   controllers: [ProfileController],
-  providers: [ProfileService, ProfileRepository],
+  providers: [ProfileService, ProfileRepository, CreateProfileUseCase],
   exports: [ProfileRepository],
 })
 export class ProfileModule {}
