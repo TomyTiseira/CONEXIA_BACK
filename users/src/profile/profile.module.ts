909db5b1e0cb218496de212b84005455e2d2d0df
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-09 20:40:08 -0300

    feat: Implementar caso de uso para obtener perfil de usuario y agregar DTO para validación de datos.

--- users/src/profile/profile.module.ts
+++ users/src/profile/profile.module.ts
@@ -5,11 +5,12 @@ import { ProfileController } from './controller/profile.controller';
 import { Profile } from './entities/profile.entity';
 import { ProfileRepository } from './repository/profile.repository';
 import { ProfileService } from './service/profile.service';
+import { GetProfileUseCase } from './service/use-cases/get-profile.use-cases';
 
 @Module({
   imports: [SharedModule, TypeOrmModule.forFeature([Profile])],
   controllers: [ProfileController],
-  providers: [ProfileService, ProfileRepository],
-  exports: [ProfileRepository],
+  providers: [ProfileService, ProfileRepository, GetProfileUseCase],
+  exports: [ProfileRepository, ProfileService],
 })
 export class ProfileModule {}
