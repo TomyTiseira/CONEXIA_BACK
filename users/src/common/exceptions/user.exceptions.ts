437cac55903e0313f78458c44603d6e812c82bb6
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-11 10:40:02 -0300

    feat: añadir UserBadRequestException para manejar errores de solicitud incorrecta del usuario

--- users/src/common/exceptions/user.exceptions.ts
+++ users/src/common/exceptions/user.exceptions.ts
@@ -89,3 +89,12 @@ export class RoleNotFoundException extends RpcException {
     });
   }
 }
+
+export class UserBadRequestException extends RpcException {
+  constructor(message: string) {
+    super({
+      status: 400,
+      message,
+    });
+  }
+}
