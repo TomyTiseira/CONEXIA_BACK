b68bd18149fa25edaec1d0cc0ae7fa8b85b75104
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-15 17:27:10 -0300

    feat: Reestructurar UpdateUserDto para incluir validaciones de userId, password y confirmPassword, mejorando la seguridad y la integridad de los datos.

--- users/src/users/dto/update-user.dto.ts
+++ users/src/users/dto/update-user.dto.ts
@@ -1,6 +1,45 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsStrongPassword,
+  Validate,
+} from 'class-validator';
+import { MatchConstraint } from '../../validators/match.validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  id: number;
+export class UpdateUserDto {
+  @IsNumber({}, { message: 'userId must be a number' })
+  @IsNotEmpty({ message: 'userId is required' })
+  userId: number;
+
+  @IsString({ message: 'password must be a string' })
+  @IsNotEmpty({ message: 'password is required' })
+  @IsStrongPassword({
+    minLength: 12,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  password: string;
+
+  @IsString({ message: 'confirmPassword must be a string' })
+  @IsNotEmpty({ message: 'confirmPassword is required' })
+  @IsStrongPassword(
+    {
+      minLength: 12,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'confirmPassword must contain at least one uppercase letter, one lowercase letter, one number and one special character',
+    },
+  )
+  @Validate(MatchConstraint, ['password'], {
+    message: 'passwords do not match',
+  })
+  confirmPassword: string;
 }
