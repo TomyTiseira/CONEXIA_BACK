3b1699b41e664e0cb9926f82c2cfcc8ad7119fe8
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-16 20:07:19 -0300

    fix: Actualizar la clase DeleteUserDto para incluir validación del ID del usuario como número. Se mejora la validación de datos en la eliminación de usuarios en la API.

--- users/src/users/dto/delete-user.dto.ts
+++ users/src/users/dto/delete-user.dto.ts
@@ -1,7 +1,13 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class DeleteUserDto {
-  
+  @IsNumber(
+    { allowNaN: false, allowInfinity: false },
+    { message: 'user id must be a number' },
+  )
+  @IsNotEmpty()
+  userId: number;
+
   @IsString({ message: 'reason must be a string' })
   @IsNotEmpty()
   reason: string;
