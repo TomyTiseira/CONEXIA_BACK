412b2872ad0c237502f681fac9ee96d4883bbb6b
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-29 14:08:03 -0300

    feat: actualizar el módulo compartido para incluir las entidades Skill y ProfileSkill, así como sus repositorios y el SkillsController, mejorando la gestión de habilidades en la aplicación

--- users/src/shared/shared.module.ts
+++ users/src/shared/shared.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SkillsController } from './controller/skills.controller';
 import { DocumentType } from './entities/document-type.entity';
+import { ProfileSkill } from './entities/profile-skill.entity';
 import { Role } from './entities/role.entity';
+import { Skill } from './entities/skill.entity';
 import { User } from './entities/user.entity';
+import { ProfileSkillRepository } from './repository/profile-skill.repository';
+import { SkillRepository } from './repository/skill.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role, DocumentType])],
-  exports: [TypeOrmModule.forFeature([User, Role, DocumentType])],
+  imports: [
+    TypeOrmModule.forFeature([User, Role, DocumentType, Skill, ProfileSkill]),
+  ],
+  controllers: [SkillsController],
+  providers: [SkillRepository, ProfileSkillRepository],
+  exports: [
+    TypeOrmModule.forFeature([User, Role, DocumentType, Skill, ProfileSkill]),
+    SkillRepository,
+    ProfileSkillRepository,
+  ],
 })
 export class SharedModule {}
