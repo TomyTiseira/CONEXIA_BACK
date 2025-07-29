35bd24e31ec33c7d861443d847232728b371767d
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-29 13:02:18 -0300

    refactor: eliminar el archivo CreateProfileResponseDto ya que no se utiliza en la gestión de perfiles

--- users/src/profile/dto/create-profile-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../../shared/entities/user.entity';
-import { Profile } from '../entities/profile.entity';
-
-export class CreateProfileResponseDto {
-  success: boolean;
-  message: string;
-  profile: Profile;
-  user: User;
-}
