0963c94ce373c3f75bee1ed53a0357743057e8a5
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-07-30 22:54:40 -0300

    feat: agregar módulo de proyectos con controlador y DTO para la publicación de proyectos, mejorando la gestión de proyectos en la aplicación

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { InternalUsersModule } from './internal-users/internal-users.module';
+import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, InternalUsersModule],
+  imports: [UsersModule, AuthModule, InternalUsersModule, ProjectsModule],
   controllers: [],
   providers: [],
 })
---
+++ api-gateway/src/projects/projects.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { ROLES } from 'src/auth/constants/role-ids';
+import { AuthRoles } from 'src/auth/decorators/auth-roles.decorator';
+import { User } from 'src/auth/decorators/user.decorator';
+import { AuthenticatedUser } from 'src/common/interfaces/authenticatedRequest.interface';
+import { NATS_SERVICE } from '../config';
+import { PublishProjectDto } from './dtos/publish-project.dto';
+
+@Controller('projects')
+export class ProjectsController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Get('ping')
+  ping() {
+    return this.client.send('ping', {}).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @AuthRoles([ROLES.USER])
+  @Post('publish')
+  publishProject(
+    @Body() publishProjectDto: PublishProjectDto,
+    @User() user: AuthenticatedUser,
+  ) {
+    return this.client
+      .send('publishProject', {
+        ...publishProjectDto,
+        userId: user.id,
+      })
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
+  }
+}
