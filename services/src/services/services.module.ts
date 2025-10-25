eeffefa4284d92793193b6a70da5bed671d1c81b
alexParedes027 <alexparedes027@gmail.com>
2025-10-25 15:08:12 -0300

    feat(reviews): add reviews information to transformed services and update use cases

--- services/src/services/services.module.ts
+++ services/src/services/services.module.ts
@@ -2,6 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from '../common/common.module';
 import { ServiceHiringsModule } from '../service-hirings/service-hirings.module';
+import { ServiceReviewsModule } from '../service-reviews/service-reviews.module';
 import { ServicesController } from './controllers';
 import { Service, ServiceCategory } from './entities';
 import { ServiceRepository } from './repositories';
@@ -20,6 +21,7 @@ import { GetServicesUseCase } from './services/use-cases/get-services.use-case';
     TypeOrmModule.forFeature([Service, ServiceCategory]),
     CommonModule,
     forwardRef(() => ServiceHiringsModule),
+    forwardRef(() => ServiceReviewsModule),
   ],
   controllers: [ServicesController],
   providers: [
